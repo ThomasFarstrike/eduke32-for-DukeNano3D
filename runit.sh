8b073fe9776c7793f5f49813f06c8159732a8f8e
Thomas Farstrike <thomasverstreken@protonmail.com>
2026-06-09 09:50:20 +0200

    runit

--- runit.sh
+++ runit.sh
@@ -1,4 +1,5 @@
 #./eduke32 -usecwd -g newfile.grp 
 #./eduke32 -usecwd -g newfile.grp -l2
 #./eduke32 -usecwd -g E1L1-6_compromise.grp -l2
-./eduke32 -usecwd -g ../outputs/E1L1-6_nearcomplete.grp
+#./eduke32 -usecwd -g ../outputs/E1L1-6_nearcomplete.grp
+./eduke32 -usecwd -g ../outputs/E1L1-6_compromise.grp
