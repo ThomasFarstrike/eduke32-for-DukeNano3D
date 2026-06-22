33bce8205a726ca05da678cf1a4aaca630beb2cd
Thomas Farstrike <thomasverstreken@protonmail.com>
2026-06-22 19:54:28 +0200

    changes

--- runit.sh
+++ runit.sh
@@ -2,11 +2,11 @@
 #./eduke32 -usecwd -g newfile.grp -l2
 #./eduke32 -usecwd -g E1L1-6_compromise.grp -l2
 #./eduke32 -usecwd -g ../outputs/E1L1-6_nearcomplete.grp
-#./eduke32 -usecwd -g ../outputs/E1L1-6_compromise.grp
+#./eduke32 -usecwd -g ../outputs/E1L1-6_compromise.grp $@
 #./eduke32 -usecwd -g ../outputs_precalculated_pngs_pngquant_40-71_2/E1L1.grp
 #./eduke32 -usecwd -g ../outputs_starryskyline_fixpipebomb/E1L1-2_compromise.grp
 #./eduke32 -usecwd -g ../outputs_onlysmaller_replaceskyline/E1L1-4_heavycompromise.grp
-./eduke32 -usecwd -g ../outputs_fix_title_images/E1L1-4_compromise.grp
+#./eduke32 -usecwd -g ../outputs_fix_title_images/E1L1-4_compromise.grp $@
 
 # original:
-#./eduke32 -usecwd -g ../input/DUKE3D_v1.3d_shareware.grp
+./eduke32 -usecwd -g ../input/DUKE3D_v1.3d_shareware.grp $@
